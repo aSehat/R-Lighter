d4b6a2a910043b2115aafa039fa1326290ef263d
blin45 <71313807+blin45@users.noreply.github.com>
2020-10-10 20:37:06 -0400

    initialpush

---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Logo from './rdf-beatles.png';
+function Home() {
+  return (
+    <home>
+        <h2>GET STARTED</h2>
+        <img src ={Logo} alt = "website logo" />;
+    </home>
+
+  );
+}
+
+export default Home;
---
+++ frontend/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <nav>
+      <h1>RDF-Highlighter</h1>
+      <ul className="nav-bar">
+          <li>Home</li>
+          <li>Create Project</li>
+          <li>Login</li>
+          <li>Sign up</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
