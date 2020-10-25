7548c567940725e10101eebc10c116541fabbb4b
emiliearenkid <43757349+emiliearenkid@users.noreply.github.com>
2020-10-25 13:33:45 -0400

    Moved stylesheet from App.js to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,10 +17,6 @@ function App() {
             <Route path="/PDFHighlights" exact component={PDFHighlights} />
           </Switch>
         </div>
-        <link rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-                crossOrigin="anonymous"/>
       </Router>
       
   );
