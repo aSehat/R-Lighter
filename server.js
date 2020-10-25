2fc01c7845793fcc853451d4b659fad5324658e2
aSehat <47791263+aSehat@users.noreply.github.com>
2020-10-25 19:31:00 -0400

    Annotation, resource, project models + routes for project

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({extended: false}));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/project', require('./routes/api/project'));
 
 const PORT = process.env.PORT || 5000;
 
