76660a06ad84f4ba0deec98c241433a28227b434
Gio <tremag@rpi.edu>
2020-11-01 12:27:50 -0800

    good commit message

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,38 +1,42 @@
 import React, { useState } from "react";
 import DashboardHeader from "./DashboardHeader";
 import DashboardListHeader from "./DashboardListHeader";
+import ProjectList from "./DashboardProjectList";
 import {axios} from 'axios';
 
 
 // this function will be an axios call to one of the routes (ask ace how to differentiate between the '/' endpoints)
-function queryDB() {
+function getProjects() {
   //
 }
 
-
-// implementation of infinite scroll
-function dynamicLoad() {
+// call '/me' endpoint
+function getUser() {
   //
 }
+
 
 export default function Dashboard(props) {
   // the initial value for projects is very expensive to execute, so put it in a function (apparently React stops
   //  unnecessary functions before they execute, but not unnecessary statements)
   const [projects, setProjects] = useState(() => {
-    // pull from db and generate a bunch of li elements
+    // TODO: pull from db and generate a bunch of li elements
   });
+
   // to avoid ambiguity, ascending means to begin with the smallest/first element (0->9, A->Z, etc)
   const [sortBy, setSortBy] = useState({
     attribute: "name",
     ascending: true,
   });
 
-  // TODO: figure out how to pass the current user to PageHeader after having logged in
+  // I don't think this is necessary, since a username will never change without a forced logout and redirect
+  // const [user, setUser] = useState(() => getUser());
+
   return (
     <div>
-      <DashboardHeader user={"HARDCODED USER"}/>
-      <DashboardListHeader sortby={sortBy} togglesort={() => this.toggleSort()}/>
-      <ol>{projects}</ol>
+      <DashboardHeader user={() => getUser()}/>
+      <DashboardListHeader sortby={sortBy} altersort={() => this.toggleSort()}/>
+      <ProjectList projects={projects}/>
     </div>
   );
 }
---
+++ frontend/src/components/DashboardProjectList.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+
+// implementation of infinite scroll
+function dynamicLoad() {
+  //
+}
+
+
+export default function DashboardProjectList(props) {
+  return (
+    <div>test</div>
+  );
+}
