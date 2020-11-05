c31abe22bbf6cdae0ef73c89de479727993450d2
Gio <tremag@rpi.edu>
2020-11-05 11:28:08 -0800

    rendered table rows functionality coming soon loading from db coming soon

--- frontend/src/components/DashboardProjectList.js
+++ frontend/src/components/DashboardProjectList.js
@@ -1,15 +1,44 @@
 import React, { useState } from "react";
+import { useTable } from 'react-table';
 
 
 
-// implementation of infinite scroll
-function dynamicLoad() {
-  //
-}
+
+
+// NOTE: the list keys are made using the following 2 assumptions:
+      //  1: the "date" field is date of creation, NOT date last modified
+      //  2: it is not possible to create 2 identical projects within 1ms of each other
+      // let key = current.date + current.link;
+      // return (
+      //   <li key={key}></li>
+      // );
+
 
 
 export default function DashboardProjectList(props) {
   return (
-    <div>test</div>
+    <tbody {...props.gettablebodyprops()}>
+      {
+        props.rows.map((row) => {
+          // TODO: find out what this sorcery entails
+          props.preparerow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {
+                row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {
+                        cell.render('Cell')
+                      }
+                    </td>
+                  );
+                })
+              }
+            </tr>
+          );
+        })
+      }
+    </tbody>
   );
 }
