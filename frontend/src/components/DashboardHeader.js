641d56c24478402dba7fbb4f2dbd679791ae3569
Gio <tremag@rpi.edu>
2020-11-05 10:12:35 -0800

    header skeleton done not sure if this will be ultimately necessary

--- frontend/src/components/DashboardHeader.js
+++ frontend/src/components/DashboardHeader.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import {Link} from 'react-router-dom';
+import SearchBar from "./SearchBar";
 
 export default function DashboardHeader(props) {
+  // idk if this is necessary, might be nice
+  // let user_info = props.getuser();
+  // console.log(user_info);
   return (
-    <div>test</div>
+    <div className="inline">
+      <Link to='/PDFHighlights'>
+        <button>Create Project</button>
+      </Link>
+      <SearchBar query={() => props.setquery()}></SearchBar>
+      <p>logout functionality here</p>
+    </div>
   );
 }
